90a00582605a663e0bf53525d7a9880c9c138477
lumikangas <lumikangas@gmail.com>
2026-09-15 20:11:16 +0300

    Refactor LibRaw source collection for Nikon NEF

--- build_nikon.sh
+++ build_nikon.sh
@@ -78,25 +78,110 @@ fi
 
 # 4. Create wasm output dir
 mkdir -p wasm
+# 5. Use LibRaw's official library source list from Makefile.am.
+# Do NOT compile every .cpp under src/: LibRaw contains alternative/
+# helper implementations that must not all be linked together.
 
-# 5. Collect LibRaw sources - keep strictly to what Nikon NEF needs for first test
-# For first test we compile core + all decoders/demosaic to avoid missing symbols
-# This is still tiny and strictly for Nikon NEF diagnostic, no redesign
+LIBRAW_SRC=(
+  "$LIBRAW_DIR/src/libraw_c_api.cpp"
+  "$LIBRAW_DIR/src/libraw_datastream.cpp"
 
-LIBRAW_SRC=$(find "$LIBRAW_DIR/src" -name "*.cpp" 2>/dev/null | tr '\n' ' ')
-if [ -z "$LIBRAW_SRC" ]; then
-  echo "ERROR: No .cpp files found in $LIBRAW_DIR/src"
-  exit 1
-fi
+  "$LIBRAW_DIR/src/decoders/canon_600.cpp"
+  "$LIBRAW_DIR/src/decoders/crx.cpp"
+  "$LIBRAW_DIR/src/decoders/pana8.cpp"
+  "$LIBRAW_DIR/src/decoders/decoders_dcraw.cpp"
+  "$LIBRAW_DIR/src/decoders/sonycc.cpp"
+  "$LIBRAW_DIR/src/decompressors/losslessjpeg.cpp"
+  "$LIBRAW_DIR/src/decoders/decoders_libraw_dcrdefs.cpp"
+  "$LIBRAW_DIR/src/decoders/olympus14.cpp"
+  "$LIBRAW_DIR/src/decoders/decoders_libraw.cpp"
+  "$LIBRAW_DIR/src/decoders/dng.cpp"
+  "$LIBRAW_DIR/src/decoders/fp_dng.cpp"
+  "$LIBRAW_DIR/src/decoders/fuji_compressed.cpp"
+  "$LIBRAW_DIR/src/decoders/generic.cpp"
+  "$LIBRAW_DIR/src/decoders/kodak_decoders.cpp"
+  "$LIBRAW_DIR/src/decoders/load_mfbacks.cpp"
+  "$LIBRAW_DIR/src/decoders/smal.cpp"
+  "$LIBRAW_DIR/src/decoders/unpack_thumb.cpp"
+  "$LIBRAW_DIR/src/decoders/unpack.cpp"
 
-echo "LibRaw sources: $(echo $LIBRAW_SRC | wc -w) files"
+  "$LIBRAW_DIR/src/demosaic/aahd_demosaic.cpp"
+  "$LIBRAW_DIR/src/demosaic/ahd_demosaic.cpp"
+  "$LIBRAW_DIR/src/demosaic/dcb_demosaic.cpp"
+  "$LIBRAW_DIR/src/demosaic/dht_demosaic.cpp"
+  "$LIBRAW_DIR/src/demosaic/misc_demosaic.cpp"
+  "$LIBRAW_DIR/src/demosaic/xtrans_demosaic.cpp"
+
+  "$LIBRAW_DIR/src/integration/dngsdk_glue.cpp"
+  "$LIBRAW_DIR/src/integration/rawspeed_glue.cpp"
+
+  "$LIBRAW_DIR/src/metadata/adobepano.cpp"
+  "$LIBRAW_DIR/src/metadata/canon.cpp"
+  "$LIBRAW_DIR/src/metadata/ciff.cpp"
+  "$LIBRAW_DIR/src/metadata/cr3_parser.cpp"
+  "$LIBRAW_DIR/src/metadata/epson.cpp"
+  "$LIBRAW_DIR/src/metadata/exif_gps.cpp"
+  "$LIBRAW_DIR/src/metadata/fuji.cpp"
+  "$LIBRAW_DIR/src/metadata/identify_tools.cpp"
+  "$LIBRAW_DIR/src/metadata/identify.cpp"
+  "$LIBRAW_DIR/src/metadata/kodak.cpp"
+  "$LIBRAW_DIR/src/metadata/leica.cpp"
+  "$LIBRAW_DIR/src/metadata/makernotes.cpp"
+  "$LIBRAW_DIR/src/metadata/mediumformat.cpp"
+  "$LIBRAW_DIR/src/metadata/minolta.cpp"
+  "$LIBRAW_DIR/src/metadata/misc_parsers.cpp"
+  "$LIBRAW_DIR/src/metadata/nikon.cpp"
+  "$LIBRAW_DIR/src/metadata/normalize_model.cpp"
+  "$LIBRAW_DIR/src/metadata/olympus.cpp"
+  "$LIBRAW_DIR/src/metadata/hasselblad_model.cpp"
+  "$LIBRAW_DIR/src/metadata/p1.cpp"
+  "$LIBRAW_DIR/src/metadata/pentax.cpp"
+  "$LIBRAW_DIR/src/metadata/samsung.cpp"
+  "$LIBRAW_DIR/src/metadata/sony.cpp"
+  "$LIBRAW_DIR/src/metadata/tiff.cpp"
+
+  "$LIBRAW_DIR/src/postprocessing/aspect_ratio.cpp"
+  "$LIBRAW_DIR/src/postprocessing/dcraw_process.cpp"
+  "$LIBRAW_DIR/src/postprocessing/mem_image.cpp"
+  "$LIBRAW_DIR/src/postprocessing/postprocessing_aux.cpp"
+  "$LIBRAW_DIR/src/postprocessing/postprocessing_utils_dcrdefs.cpp"
+  "$LIBRAW_DIR/src/postprocessing/postprocessing_utils.cpp"
+
+  "$LIBRAW_DIR/src/preprocessing/ext_preprocess.cpp"
+  "$LIBRAW_DIR/src/preprocessing/raw2image.cpp"
+  "$LIBRAW_DIR/src/preprocessing/subtract_black.cpp"
+
+  "$LIBRAW_DIR/src/tables/cameralist.cpp"
+  "$LIBRAW_DIR/src/tables/colorconst.cpp"
+  "$LIBRAW_DIR/src/tables/colordata.cpp"
+  "$LIBRAW_DIR/src/tables/wblists.cpp"
+
+  "$LIBRAW_DIR/src/utils/curves.cpp"
+  "$LIBRAW_DIR/src/utils/decoder_info.cpp"
+  "$LIBRAW_DIR/src/utils/init_close_utils.cpp"
+  "$LIBRAW_DIR/src/utils/open.cpp"
+  "$LIBRAW_DIR/src/utils/phaseone_processing.cpp"
+  "$LIBRAW_DIR/src/utils/read_utils.cpp"
+  "$LIBRAW_DIR/src/utils/thumb_utils.cpp"
+  "$LIBRAW_DIR/src/utils/utils_dcraw.cpp"
+  "$LIBRAW_DIR/src/utils/utils_libraw.cpp"
+
+  "$LIBRAW_DIR/src/write/apply_profile.cpp"
+  "$LIBRAW_DIR/src/write/file_write.cpp"
+  "$LIBRAW_DIR/src/write/tiff_writer.cpp"
+
+  "$LIBRAW_DIR/src/x3f/x3f_parse_process.cpp"
+  "$LIBRAW_DIR/src/x3f/x3f_utils_patched.cpp"
+)
+
+echo "LibRaw library sources: ${#LIBRAW_SRC[@]} files"
 echo ""
 
 # 6. Build - Emscripten emits WASM by default, no special WASM mode needed
 echo "Building wasm/lumiGoldRaw.js ..."
 echo ""
 
-emcc $LIBRAW_SRC \
+emcc "${LIBRAW_SRC[@]}" \
   ./lumiGoldRawWrapper.cpp \
   -I"$LIBRAW_DIR" \
   -I"$LIBRAW_DIR/src" \
